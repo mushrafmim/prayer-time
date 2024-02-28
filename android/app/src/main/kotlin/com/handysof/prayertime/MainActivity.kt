96986e7e51aaf74132ce185bab4d87183428a208
Mohamed Mushraf <mushrafmim@gmail.com>
2024-02-28 16:46:40 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/handysof/prayertime/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.handysof.prayertime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/Colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xFF004366e);
+  static const secondaryColor = Color(0xFF00608e);
+  static const accentColor = Color(0xFF007BA8e);
+  static const backgroundColor = Color(0xFFE5E5E5);
+}
